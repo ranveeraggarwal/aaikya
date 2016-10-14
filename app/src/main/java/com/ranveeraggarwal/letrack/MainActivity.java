ba2a2a4253c7eec8d67b210ae988b37d3109a7ee
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2016-10-14 17:45:02 +0530

    UI mods

--- app/src/main/java/com/ranveeraggarwal/letrack/MainActivity.java
+++ app/src/main/java/com/ranveeraggarwal/letrack/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         int[] frequencies = {2, 1, 3, 2, 1};
         int[] leaves = {1, 4, 0, 0, 10};
         for (int i=0; i<names.length && i<occupations.length && i<frequencies.length && i<leaves.length; i++) {
-            Person person = new Person(names[i], frequencies[i], occupations[i], leaves[i]);
+            Person person = new Person(names[i], frequencies[i], occupations[i], leaves[i], 1);
             data.add(person);
         }
 
--- app/src/main/java/com/ranveeraggarwal/letrack/models/Person.java
+++ app/src/main/java/com/ranveeraggarwal/letrack/models/Person.java
@@ -8,20 +8,23 @@ public class Person {
     private String name;
     private int frequency;
     private int leaves;
+    private int startDate;
     private String occupation;
 
     public Person () {}
-    public Person (String name, int frequency, String occupation) {
+    public Person (String name, int frequency, String occupation, int startDate) {
         this.setName(name);
         this.setFrequency(frequency);
         this.setOccupation(occupation);
         this.setLeaves(0);
+        this.setStartDate(startDate);
     }
-    public Person (String name, int frequency, String occupation, int leaves) {
+    public Person (String name, int frequency, String occupation, int leaves, int startDate) {
         this.setName(name);
         this.setFrequency(frequency);
         this.setOccupation(occupation);
         this.setLeaves(leaves);
+        this.setStartDate(startDate);
     }
 
     public String getName() {
@@ -54,5 +57,13 @@ public class Person {
 
     public void setOccupation(String occupation) {
         this.occupation = occupation;
+    }
+
+    public int getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(int startDate) {
+        this.startDate = startDate;
     }
 }
